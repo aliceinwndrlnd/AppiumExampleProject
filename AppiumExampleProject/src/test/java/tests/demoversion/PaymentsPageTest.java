8cec5603e5106f4cfdf3ab53237fa990ecaf8c16
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2022-07-08 22:35:36 +0300

    added the new tests dataprovider

--- AppiumExampleProject/src/test/java/tests/demoversion/PaymentsPageTest.java
+++ AppiumExampleProject/src/test/java/tests/demoversion/PaymentsPageTest.java
@@ -3,9 +3,11 @@ package tests.demoversion;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class PaymentsPageTest extends DemoVersionBaseTest {
+    protected final String bikSearchFieldValue = "Сбербанк";
 
     @BeforeClass
     public void toGoToPaymentsPage() {
@@ -27,6 +29,32 @@ public class PaymentsPageTest extends DemoVersionBaseTest {
         paymentsPage.tapToPaymentOfUtilityServices()
                 .checkErrorMessage()
                 .tapOkButton();
+    }
+
+    @Description("Payments by requisites")
+    @Test(priority = 3, dataProvider = "requisites")
+    public void payingByRequisitesTest(String recipientName, String recipientAccount, String inn, String kpp, String transferSum) {
+        paymentsPage.tapByRequisitesPayment();
+        paymentByRequisitesPage.fillTheRecipientNameField(recipientName)
+                .fillTheRecipientAccountField(recipientAccount)
+                .fillTheInnField(inn)
+                .fillTheKppField(kpp)
+                .tapBikField()
+                .fillTheBikField(bikSearchFieldValue)
+                .chooseBank()
+                .scrollToTheTransferSumField()
+                .checkThePaymentAppointmentField()
+                .fillTheTransferSumField(transferSum)
+                .tapTransferButton()
+                .checkErrorMessage()
+                .tapOkButton();
+    }
+
+    @DataProvider(name = "requisites")
+    private Object[][] getRequisites() {
+        return new Object[][]{
+                {"Tester", "123456", "111", "222", "1000"},
+        };
     }
 }
 
